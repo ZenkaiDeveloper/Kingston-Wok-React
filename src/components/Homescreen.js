cc2a4b55536ac96ed73e428e9be6b294735247d2
Andy <wang.andy32@gmail.com>
2018-12-26 13:05:51 -0500

    added desktop landing page with a slider that does not show on mobile. Also hid content on mobile with CSS

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -8,12 +8,15 @@ import HomePageInfo from './HomePageInfo'
 
 class Homescreen extends Component{
   state={
-    x: 0
+    x: 0,
+    skew:1000,
+    delta:0
   }
   changeHandler = (event) => {
     console.log("X Value :", event.screenX, "Y value: ",event.screenY)
     this.setState({
-      x:event.screenX
+      x:event.clientX,
+      delta: (event.clientX - window.innerWidth/2) * 0.3
     })
   }
   render(){
@@ -27,26 +30,28 @@ class Homescreen extends Component{
         </div>
 
         <section onMouseMove={this.changeHandler} className="showcase">
-           <h1 class="catcher">Asian Fusion Restaurant</h1>
-           <div class="layer bottom">
-             <div class="content-wrap">
-               <div class="content-body">
+          <img alt="Chinese Noodles" className="show-img-mobile" src={chineseNoodle} />
+
+           <h1 className="catcher">Asian Fusion Restaurant</h1>
+           <div className="layer bottom">
+             <div className="content-wrap">
+               <div className="content-body">
                  <h1>Chinese Cusine</h1>
                  <p>Enjoy classic chinese flavors.</p>
                </div>
                <img alt="Chinese Noodles" className="show-img" src={chineseNoodle} />
              </div>
            </div>
-           <div class="layer top">
-             <div class="content-wrap">
-               <div class="content-body">
+           <div style={{width:`${this.state.x + this.state.skew + this.state.delta}px`}} className="layer top">
+             <div className="content-wrap">
+               <div className="content-body">
                  <h1>Japanese Cusine</h1>
                  <p>Take your tastebuds on a journey through Japan.</p>
                </div>
                <img alt="Japanese Sashimi" className="show-img2" src={sushiImg} />
              </div>
            </div>
-           <div class="handle"></div>
+           <div style={{left:`${this.state.x + this.state.delta}px`}} className="handle"></div>
         </section>
         <HomePageInfo />
       </div>
